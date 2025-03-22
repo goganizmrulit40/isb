836d14ffe02aa5239fdce904a5c40864ddd33628
Maxim <m4ximyarosl@yandex.ru>
2025-03-22 17:55:02 +0400

    добавим считывание текст из файла и запись в другой файл дешифрованного текста

--- lab_1/main_1.py
+++ lab_1/main_1.py
@@ -1,37 +1,4 @@
-import string
 from collections import Counter
-
-cipher_text = """
-=73КЙИZКРИИNИ@О=гQZiЯХN@И@ХТуiNiО7КЖгТЙИGNЖТ7@ИGуИ
-NJгGNО7ТJ=ХЖ7КYТЙИGNЖЙТ@ТЙИGN@гi1КFЙU<NИZуТЙТЙИFNК
-ЖХ=7ИZ=ЖКЙЙUуИNО=гQZ=ЖКХТгGуИ
-JЖКNО=гQZ=ЖКХТгИNЙТNу=3iХN=ПЙ=ЖгGХQN=JИЙNИNХ=ХNRТN=
-ПsТ%Х
-=ПsТ%ХКуИNу=3iХNПUХQNДКFгUNZКОИ@ИNПКZUNJКЙЙU<NИгИN
-Х7КЙZК%РИИ
-ЙКО7ИуТ7NОТ7ЖUFNО=гQZ=ЖКХТгQN=Х%7UЖКЯYИFNJ=%iуТЙХNИ
-уТТХN7КZ7ТюТЙИТNЙКNИZуТЙТЙИТNЁХ=3=NJ=%iуТЙХКNNЖХ=7=FN
-О=гQZ=ЖКХТгQNИуТТХN7КZ7ТюТЙИТNХ=гQ%=NЙКN1ХТЙИТ
-Х=1ЙUТN7ТZТ7ЖЙUТN%=ОИИNО=у=3КЯХN@=<7КЙИХQNРТг=@ХЙ=
-@ХQNJКЙЙU<NЖN@гi1КТNО=Ж7ТRJТЙИGNJКЙЙU<
-=73КЙИZКРИGуN@гТJiТХN%=ЙХ7=гИ7=ЖКХQNО7=РТ@@N7ТZТ7ЖЙ=
-3=N%=ОИ7=ЖКЙИGN1Х=ПUN3К7КЙХИ7=ЖКХQNРТг=@ХЙ=@ХQN7ТZ
-Т7ЖЙU<N%=ОИFNИNО7ТJ=ХЖ7КYТЙИТNО=ХТ7ИNJКЙЙU<
-КЖХ=7ИZКРИGN=О7ТJТгGТХN%Х=NИуТТХNJ=@ХiОN%N7Т@i7@КуN
-=73КЙИZКРИИNЙКN=@Й=ЖТNЖUО=гЙGТуU<NЖN=73КЙИZКРИИN=П
-GZКЙЙ=@ХТF
-ЙКО7ИуТ7N@NО=у=YQЯNО7КЖNJ=@ХiОКN%NДКFгКуNИN@7ТJ@ХЖ
-N7КZ37КЙИ1ТЙИGNJ=@ХiОКN=ПТ@ОТ1ИЖКТХ@GN3К7КЙХИGNХ=3
-=N1Х=NХ=гQ%=N=О7ТJТгТЙЙUТNО=гQZ=ЖКХТгИNу=3iХNИZуТЙGХQ
-NJКЙЙUТ
-КJуИЙИ@Х7КХ=7Nу=RТХNi@ХКЙ=ЖИХQNО7КЖКNJ=@ХiОКN%NДКF
-гiNХ=гQ%=NЙКN1ХТЙИТ
-ЖN7ТZiгQХКХТNО=гQZ=ЖКХТгQN%=Х=7UFNО=гi1КТХNJ=@ХiОN%N
-ЁХ=уiNДКFгiNЙТNу=RТХNЖЙТ@ХИNЖNДКFгNЙИ%К%И<NИZуТЙТЙ
-ИF
-"""
-
-cipher_text = cipher_text.replace('\n', '')
 
 frequency_index = {
     ' ': 0.128675,
@@ -69,13 +36,26 @@ frequency_index = {
     'Ъ': 0.000000
 }
 
-frequency = Counter(cipher_text)
+def main():
+    with open('cod23.txt', 'r', encoding='utf-8') as file:
+        text = file.read()
 
-total_chars = sum(frequency.values())
-frequency_index_cipher = {char: count / total_chars for char, count in frequency.items()}
+    print(text)
 
-sorted_frequency_cipher_index = dict(sorted(frequency_index_cipher.items(), key=lambda item: item[1], reverse=True))
+    print("\n++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n")
+    frequency = Counter(text)
 
-print("Индекс частоты появления букв:")
-for char, freq in sorted_frequency_cipher_index.items():
-    print(f"Символ: '{char}', Частота: {freq:.6f}")
+    total_chars = sum(frequency.values())
+    frequency_index_cipher = {char: count / total_chars for char, count in frequency.items()}
+
+    sorted_frequency_cipher_index = dict(sorted(frequency_index_cipher.items(), key=lambda item: item[1], reverse=True))
+
+    print("Индекс частоты появления букв:")
+    for char, freq in sorted_frequency_cipher_index.items():
+        print(f"Символ: '{char}', Частота: {freq:.6f}")
+
+    with open('res.txt', 'w', encoding='utf-8') as file:
+        file.write(text)
+
+if __name__ == "__main__":
+    main()
