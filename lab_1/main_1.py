d9bafec38caef1dadd7a93107b051bb181785b6e
Maxim <m4ximyarosl@yandex.ru>
2025-03-25 22:24:49 +0400

    Обернул в функции чтение из файла (read_from_file) и запись в файлы (write_to_files) Также обернул в файл подсчёт частоты (frequency_count) REPLACEMENT_DICT_RES мне необходима в файле constants.py, поскольку именно этот словарь я и записываю как найденный ключ шифрования в один из итоговых файлов (replacement_dict.txt)
    Signed-off-by: Maxim <m4ximyarosl@yandex.ru>

--- lab_1/main_1.py
+++ lab_1/main_1.py
@@ -7,16 +7,41 @@ def replace_text(text, const):
 
     return text
 
-def main():
+def frequency_count (frequency, total_chars):
+    frequency_index_cipher = {char: count / total_chars for char, count in frequency.items()}
+    sorted_frequency_cipher_index = dict(sorted(frequency_index_cipher.items(), key=lambda item: item[1], reverse=True))
+
+    return sorted_frequency_cipher_index
+
+def read_from_file():
     try:
         with open(FILE_ENTER, 'r', encoding='utf-8') as file:
-            text = file.read()
+            return file.read()
     except FileNotFoundError:
         print(f"Ошибка: Файл '{FILE_ENTER}' не найден.")
         return
     except IOError as e: #ошибки ввода-вывода
         print(f"Ошибка при чтении файла '{FILE_ENTER}': {e}")
         return
+
+def write_to_files(text):
+    try:
+        with open(FILE_EXIT, 'w', encoding='utf-8') as file:
+            file.write(text)
+    except IOError as e:
+        print(f"Ошибка при записи в файл '{FILE_EXIT}': {e}")
+        return
+
+    try:
+        with open(FILE_DICT, 'w', encoding='utf-8') as f:
+            for key, value in REPLACEMENT_DICT_RES.items():
+                f.write(f"{key}: {value}\n")
+    except IOError as e:
+        print(f"Ошибка при записи в файл '{FILE_DICT}': {e}")
+        return
+
+def main():
+    text = read_from_file()
 
     print(text)
 
@@ -32,9 +57,7 @@ def main():
         print("Ошибка: текст пуст, невозможно вычислить частоту.")
         return
 
-    frequency_index_cipher = {char: count / total_chars for char, count in frequency.items()}
-
-    sorted_frequency_cipher_index = dict(sorted(frequency_index_cipher.items(), key=lambda item: item[1], reverse=True))
+    sorted_frequency_cipher_index = frequency_count(frequency, total_chars)
 
     print("Индекс частоты появления букв:")
     for char, freq in sorted_frequency_cipher_index.items():
@@ -46,20 +69,7 @@ def main():
     print(text)
     print("\n")
 
-    try:
-        with open(FILE_EXIT, 'w', encoding='utf-8') as file:
-            file.write(text)
-    except IOError as e:
-        print(f"Ошибка при записи в файл '{FILE_EXIT}': {e}")
-        return
-
-    try:
-        with open(FILE_DICT, 'w', encoding='utf-8') as f:
-            for key, value in REPLACEMENT_DICT_RES.items():
-                f.write(f"{key}: {value}\n")
-    except IOError as e:
-        print(f"Ошибка при записи в файл '{FILE_DICT}': {e}")
-        return
+    write_to_files(text)
 
 if __name__ == "__main__":
     main()
