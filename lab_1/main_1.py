888470ba26230c5c98d0f7138c7c95ee71cde768
Maxim <m4ximyarosl@yandex.ru>
2025-03-26 10:14:46 +0400

    Вынес в функцию обработки частоты создание словаря этой частоты и объединил это с функцией подсчёта частоты, также в этой функции идёт вывод в консоль получившегося словаря
    Signed-off-by: Maxim <m4ximyarosl@yandex.ru>

--- lab_1/main_1.py
+++ lab_1/main_1.py
@@ -9,16 +9,25 @@ def replace_text(text, const):
     return text
 
 
-def frequency_count (frequency, total_chars):
-    frequency_index_cipher = {char: count / total_chars for char, count in frequency.items()}
-    sorted_frequency_cipher_index = dict(sorted(frequency_index_cipher.items(), key=lambda item: item[1], reverse=True))
-
-    return sorted_frequency_cipher_index
-
-
 def read_from_file():
     with open(FILE_ENTER, 'r', encoding='utf-8') as file:
         return file.read()
+
+
+def processing_frequency(text):
+    frequency = Counter(text)
+
+    total_chars = sum(frequency.values())
+    if total_chars == 0:
+        print("Ошибка: текст пуст, невозможно вычислить частоту.")
+        return
+
+    frequency_index_cipher = {char: count / total_chars for char, count in frequency.items()}
+    sorted_frequency_cipher_index = dict(sorted(frequency_index_cipher.items(), key=lambda item: item[1], reverse=True))
+
+    print("Индекс частоты появления букв:")
+    for char, freq in sorted_frequency_cipher_index.items():
+        print(f"Символ: '{char}', Частота: {freq:.6f}")
 
 
 def write_to_files(text):
@@ -37,18 +46,8 @@ def main():
         print(text)
 
         print("\n++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n")
-        frequency = Counter(text)
 
-        total_chars = sum(frequency.values())
-        if total_chars == 0:
-            print("Ошибка: текст пуст, невозможно вычислить частоту.")
-            return
-
-        sorted_frequency_cipher_index = frequency_count(frequency, total_chars)
-
-        print("Индекс частоты появления букв:")
-        for char, freq in sorted_frequency_cipher_index.items():
-            print(f"Символ: '{char}', Частота: {freq:.6f}")
+        processing_frequency(text)
 
         print("\n++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n")
         text = replace_text(text, REPLACEMENT_DICT_TWO)
