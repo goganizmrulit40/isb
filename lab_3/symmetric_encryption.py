5a84d0526f37e5b0bf4417e7ffb12a9290ea2411
Maxim <m4ximyarosl@yandex.ru>
2025-06-01 13:02:52 +0400

    Теперь начнём писать шифрование текста. Создадим инициализированный вектор с шагом в 8 бит (по условию задачи), затем создадим шифр и шифратор. Шифр создаётся с помощью данного алгоритма CAST5, который использует замену и перестановку. Шифратор - с помощью CBC, который работает с XOR - каждый последующий блок комбинируется с предыдущим.

--- lab_3/symmetric_encryption.py
+++ lab_3/symmetric_encryption.py
@@ -1,9 +1,24 @@
+import os
+
 import secrets
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 
 
 class SymmetricEncryption:
+    @staticmethod
     def __init__(self, key_length):
         if key_length not in range(40, 129, 8):
             raise ValueError("Длина ключа должна быть от 40 до 128 бит с шагом 8 бит.")
 
         self.key = secrets.token_bytes(key_length // 8)
+
+
+    @staticmethod
+    def encrypt(self, inital_text):
+        init_vector = os.urandom(8)
+        cipher = Cipher(algorithms.CAST5(self.key), modes.CBC(init_vector))
+        inital_text = cipher.encryptor()
+
+
+
+
